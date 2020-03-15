9fe230da2bcfadd9a2ee2bd1a682a7c023ae0ca5
CleigloDev <carlolune@gmail.com>
2020-03-15 19:33:07 +0100

    add navigation

--- App.js
+++ App.js
@@ -8,31 +8,47 @@ import {
   StatusBar,
   Button,
 } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
 import '@react-native-firebase/firestore';
 
-const App = () => {
-
-  _testFirebase = () => {
-    firebase.firestore().collection("test").doc("1").set({
-      id: 2,
-    });
-  };
-
+function HomeScreen({ navigation }) {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Ciao</Text>
-        <Button onPress={_testFirebase} title="prova"></Button>
-      </SafeAreaView>
-    </>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Detail')} title="test"/>
+    </View>
   );
-};
+}
+
+function DetailScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')} title="test"/>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
 
 });
 
 export default App;
+
